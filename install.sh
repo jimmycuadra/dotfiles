8b7f13b590d99fb5d1ee45a1ff894092bf843443
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-03-31 14:54:40 -0700

    Use the full path to chef-solo, wherever it is.

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-type chef-solo >/dev/null 2>&1
-if [ "$?" -ne 0 ]; then
+chef_solo=$(which chef-solo)
+if [ -z "$chef_solo" ]; then
   curl -L https://www.opscode.com/chef/install.sh -o install_chef.sh
   sudo bash install_chef.sh
   rm install_chef.sh
+  chef_solo=$(which chef-solo)
 fi
 sudo mkdir -p /var/chef/cookbooks
 sudo cp -R . /var/chef/cookbooks/dotfiles
-sudo chef-solo -c solo.rb -j node.json
+sudo $chef_solo -c solo.rb -j node.json
