4858fd9974ce456ba3dddbf150ac23880ebe030f
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-02-27 19:01:32 -0800

    Add vim-json plugin.

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,5 +9,6 @@ default[:dotfiles][:vim_plugin_urls] = [
   "git://github.com/tpope/vim-fugitive.git",
   "git://github.com/skalnik/vim-vroom.git",
   "git://github.com/jgdavey/vim-railscasts.git",
-  "git://github.com/kchmck/vim-coffee-script.git"
+  "git://github.com/kchmck/vim-coffee-script.git",
+  "git://github.com/elzr/vim-json.git"
 ]
