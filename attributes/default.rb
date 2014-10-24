4ecd2685952654240f44c5e81bfbc505d8083428
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-10-23 17:36:41 -0700

    Set home attribute differently on OS X vs. Linux.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,14 @@
-default[:dotfiles] = {
-  user: "jimmy",
-  group: "staff",
-  home_path: "/Users/jimmy"
-}
+case default[:platform_family]
+when "mac_os_x"
+  default[:dotfiles] = {
+    user: "jimmy",
+    group: "staff",
+    home_path: "/Users/jimmy"
+  }
+else
+  default[:dotfiles] = {
+    user: "jimmy",
+    group: "jimmy",
+    home_path: "/home/jimmy"
+  }
+end
