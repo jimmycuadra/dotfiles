c8a04b0e116f88ecd4f598244ae46e52eaeee748
Jimmy Cuadra <jimmy@jimmycuadra.com>
2022-07-29 03:25:42 -0700

    Add and configure nvim-cmp.

---
+++ .config/nvim/lua/user/completion/config.lua
@@ -0,0 +1,47 @@
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<cr>"] = cmp.mapping.confirm(),
+    ["<tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end),
+    ["<s-tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end),
+  }),
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = "[" .. vim_item.kind .. "]"
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name] or entry.source.name
+
+      return vim_item
+    end,
+  },
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+})
---
+++ .config/nvim/lua/user/completion/packages.lua
@@ -0,0 +1,8 @@
+local Plug = vim.fn["plug#"]
+
+Plug "hrsh7th/nvim-cmp"
+Plug "hrsh7th/cmp-buffer"
+Plug "hrsh7th/cmp-path"
+Plug "hrsh7th/cmp-nvim-lsp"
+Plug "hrsh7th/cmp-nvim-lua"
+Plug "L3MON4D3/LuaSnip"
