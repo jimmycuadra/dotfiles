31a13ffe0efaa722dfc4855ac01844b383447101
Jimmy Cuadra <jimmy@jimmycuadra.com>
2022-07-29 16:37:17 -0700

    Add and configure null-ls.

--- .config/nvim/lua/user/lsp/config.lua
+++ .config/nvim/lua/user/lsp/config.lua
@@ -1,5 +1,6 @@
 local lspconfig = require("lspconfig")
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
+local null_ls = require("null-ls")
 
 local o = vim.opt
 
@@ -18,8 +19,13 @@ map('n', '[g', vim.diagnostic.goto_prev, opts)
 map('n', ']g', vim.diagnostic.goto_next, opts)
 map('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
   local buffer_opts = { silent = true, buffer = bufnr }
+
+  -- Prefer formatting from null-ls
+  if client.name == "tsserver" or client.name == "sumneko_lua" then
+    client.resolved_capabilities.document_formatting = false
+  end
 
   map("n", "K", vim.lsp.buf.hover, buffer_opts)
 
@@ -68,4 +74,21 @@ lspconfig.sumneko_lua.setup({
 lspconfig.tsserver.setup({
   on_attach = on_attach,
   capabilities = capabilities,
+})
+
+local code_actions = null_ls.builtins.code_actions
+local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+
+
+null_ls.setup({
+  sources = {
+    code_actions.eslint,
+    diagnostics.eslint,
+    diagnostics.flake8,
+    formatting.black,
+    formatting.eslint,
+    formatting.prettier,
+    formatting.stylua,
+  },
 })
--- .config/nvim/lua/user/lsp/packages.lua
+++ .config/nvim/lua/user/lsp/packages.lua
@@ -1,3 +1,5 @@
 local Plug = vim.fn["plug#"]
 
 Plug "neovim/nvim-lspconfig"
+Plug "nvim-lua/plenary.nvim" -- Needed by null-ls
+Plug "jose-elias-alvarez/null-ls.nvim"
