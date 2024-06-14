5855517d6c6aecf3a30bc04b6a44ff98339c1f76
Jimmy Cuadra <jimmy@jimmycuadra.com>
2024-06-13 23:21:41 -0700

    Replace yggdroot/indentline with lukas-reineke/indent-blankline.nvim.

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -47,8 +47,37 @@ require("lazy").setup({
     end,
   },
 
-  -- Add faint vertical lines to mark levels of indentation
-  "yggdroot/indentline",
+  -- Add vertical lines to mark levels of indentation
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      local hooks = require("ibl.hooks")
+
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      end)
+
+      require("ibl").setup({
+        indent = {
+          highlight = {
+            "RainbowRed",
+            "RainbowYellow",
+            "RainbowBlue",
+            "RainbowOrange",
+            "RainbowGreen",
+            "RainbowViolet",
+            "RainbowCyan",
+          },
+        },
+      })
+    end,
+  },
 
   -- Ignore casing for save and quit commands
   "takac/vim-commandcaps",
