b6947886f0fe506b4b15cda0d971e2d42aed4b92
Jimmy Cuadra <jimmy@jimmycuadra.com>
2025-03-28 01:28:07 -0700

    Replace deprecated call to vim.loop with vim.uv.

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,6 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
+---@diagnostic disable-next-line: undefined-field
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
